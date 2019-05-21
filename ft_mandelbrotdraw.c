57a528040ef19fe8152670702aecc20b84614220
pholster <pholster@student.codam.nl>
2019-05-21 18:01:01 +0200

    updated drawing func

--- ft_mandelbrotdraw.c
+++ ft_mandelbrotdraw.c
@@ -6,12 +6,11 @@
 /*   By: pholster <pholster@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/05/16 12:58:21 by pholster       #+#    #+#                */
-/*   Updated: 2019/05/21 17:24:39 by pholster      ########   odam.nl         */
+/*   Updated: 2019/05/21 18:00:47 by pholster      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
-#include "time.h"
 
 static int	calccount(t_ftl *ftl, int count, float zx, float zy)
 {
@@ -26,27 +25,38 @@ static int	calccount(t_ftl *ftl, int count, float zx, float zy)
 	return (count);
 }
 
+static void	calczxy(float *zx, float *zy, float cx, float cy)
+{
+	float	tempzx;
+	float	tempzy;
+	float	tempx;
+
+	tempzx = *zx;
+	tempzy = *zy;
+	tempx = (tempzx * tempzx - tempzy * tempzy + cx);
+	*zy = (2 * tempzx * tempzy + cy);
+	*zx = tempx;
+}
+
 static void	drawx(t_ftl *ftl, int x, int y, float *scale)
 {
 	int		count;
-	float	cxy[2];
-	float	zxy[2];
-	float	tempx;
+	float	zx;
+	float	zy;
+	float	cx;
+	float	cy;
 
-	cxy[0] = x * scale[0] + ftl->posx;
-	cxy[1] = y * scale[1] + ftl->posy;
-	zxy[0] = 0;
-	zxy[1] = 0;
+	cx = x * scale[0] + ftl->posx;
+	cy = y * scale[1] + ftl->posy;
+	zx = 0;
+	zy = 0;
 	count = 0;
-	while (count < ftl->maxdetail &&
-		zxy[0] * zxy[0] + zxy[1] * zxy[1] < (1 << 16))
+	while (count < ftl->maxdetail && zx * zx + zy * zy < (1 << 16))
 	{
-		tempx = zxy[0] * zxy[0] - zxy[1] * zxy[1] + cxy[0];
-		zxy[1] = 2 * zxy[0] * zxy[1] + cxy[1];
-		zxy[0] = tempx;
+		calczxy(&zx, &zy, cx, cy);
 		count++;
 	}
-	ft_putpixel(ftl, x, y, calccount(ftl, count, zxy[0], zxy[1]));
+	ft_putpixel(ftl, x, y, calccount(ftl, count, zx, zy));
 }
 
 static void	drawy(t_ftl *ftl, int y, float *scale)
