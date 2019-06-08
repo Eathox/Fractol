9ff55fdd73e8802dd5e3ae8305cd9375eaa76102
pholster <pholster@student.codam.nl>
2019-06-08 12:55:51 +0200

    added pallet freeing

--- ft_ftldel.c
+++ ft_ftldel.c
@@ -6,13 +6,23 @@
 /*   By: pholster <pholster@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/05/03 11:38:14 by pholster       #+#    #+#                */
-/*   Updated: 2019/05/03 15:39:43 by pholster      ########   odam.nl         */
+/*   Updated: 2019/06/08 12:52:02 by pholster      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-void	ft_ftldel(t_ftl **ftl)
+static void	delete(t_ftl *current, void *mlx)
+{
+	if (current->mlx_window != NULL)
+		mlx_destroy_window(mlx, current->mlx_window);
+	if (current->mlx_image != NULL)
+		mlx_destroy_image(mlx, current->mlx_image);
+	ft_memdel(&(current->palette));
+	free(current);
+}
+
+void		ft_ftldel(t_ftl **ftl)
 {
 	void	*mlx;
 	t_pool	*pool;
@@ -27,11 +37,7 @@ void	ft_ftldel(t_ftl **ftl)
 		if (pool == NULL)
 			pool = current->pool;
 		next = current->next;
-		if (current->mlx_window != NULL)
-			mlx_destroy_window(mlx, current->mlx_window);
-		if (current->mlx_image != NULL)
-			mlx_destroy_image(mlx, current->mlx_image);
-		free(current);
+		delete(current, mlx);
 		current = next;
 	}
 	*ftl = NULL;
